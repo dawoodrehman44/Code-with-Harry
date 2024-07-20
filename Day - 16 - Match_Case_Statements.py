1ff0977bdcf2d33840732625e415414cb083e7bb
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-07-20 15:26:07 +0500

    Add files via upload

---
+++ Day - 16 - Match_Case_Statements.py
@@ -0,0 +1,17 @@
+x = int(input("Enter your number: "))
+match x:
+    case 0:
+        print("x is zero")
+    case 4:
+        print("case is 4")
+        # case with if condition
+    case _ if x!= 90:
+        print(x, "is not 90")
+    
+    case _ if x != 80:
+        print(x, "is not 80")
+
+    case _:
+        print(x)
+
+
