3803ed4603c90e08e83b0d4c59c6a4d34c0aafdc
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-07-20 17:04:48 +0500

    Add files via upload

---
+++ Day - 17- For_Loops.py
@@ -0,0 +1,11 @@
+# colors = ["Red", "Blue", "Green"]
+
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+# printing numbers from 1 to 20000
+
+for k in range(1, 2000, 100):
+    print(k)
