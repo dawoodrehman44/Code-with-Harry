ffc9468f49963523753c05af17aa77132d856a75
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-07-20 18:50:03 +0500

    Add files via upload

---
+++ Day_18- While_Loops.py
@@ -0,0 +1,21 @@
+# While Loop in Python
+# i = 0
+# while(i < 5):
+#     print(i)
+#     i = i + 1
+# print("Done with Loop")
+
+# i = int(input("Enter the number: "))
+# while (i <= 50):
+#     print(i)
+#     i = int(input("Enter the number: "))
+
+# print("Done with the loop")
+
+# decreased while loop
+count = 5
+while(count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("I am inside else")
