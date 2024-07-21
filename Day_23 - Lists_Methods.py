d57a7db201d766d543c7e8585c7c4688ca0719ef
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-07-21 17:09:46 +0500

    Add files via upload

---
+++ Day_23 - Lists_Methods.py
@@ -0,0 +1,13 @@
+# List methods
+l = [11, 45, 1, 2, 4, 6, 1, 1]
+
+#l.append(7)
+#l.sort(reverse=True)
+#l.reverse()
+#l.insert(1, 899)
+m = [1000, 1100, 1200]
+k = l + m
+print(k)
+#l.extend(m)
+
+print(l)
