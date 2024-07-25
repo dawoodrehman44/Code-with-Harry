cec95bb91f8095b9d65e6a6effed27071b78d777
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-07-25 15:31:51 +0500

    Add files via upload

---
+++ Day_33 - Dictionaries.py
@@ -0,0 +1,11 @@
+info = {"name": "Karan", "age": 29, "eligible": True}
+# print(info)
+# print(info.keys())
+
+# for key in info.keys():
+#     print(info[key])
+
+# for key in info.keys():
+#     print(f"the value to the corresping to the key {key} is {info[key]}")
+
+print(info.items())
