5f2c288b076081687edbb6a02c54205b26d3c907
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-07-25 15:03:48 +0500

    Add files via upload

---
+++ Day_32 - Set_Methods.py
@@ -0,0 +1,54 @@
+# s = {2, 3, 2, 4, 6}
+# print(s)
+
+# info = {"Carla", 19, False, 5.9, 19}
+# print(info)
+
+# for set in info:
+#     print(set)
+
+# #### SET METHODS IN PYTHON
+
+# cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities3 = cities.symmetric_difference(cities2)
+# print(cities3)
+
+# #Union
+# cities4 = cities.union(cities2)
+# print(cities4)
+
+# #Intersection
+# cities5 = cities.intersection(cities2)
+# print(cities5)
+# #intersection_update
+# cities1 = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities22 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# cities31 = cities1.intersection_update(cities22)
+# print(cities31)
+
+#difference amd difference update
+# cities1 = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madrid"}
+# print(cities1.difference(cities2))
+# print(cities1.difference_update(cities2))
+
+#disjoint
+# cities1 = {"Toky1o", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madr1id"}
+# cities3 = cities1.isdisjoint(cities2)
+# print(cities3)
+# #is_spersubset
+# cities1 = {"Toky1o", "Madrid", "Berlin", "Delhi"}
+# cities2 = {"Tokyo", "Seoul", "Kabul", "Madr1id"}
+# print(cities1.issuperset(cities2))
+
+# add function
+# cities1 = {"Toky1o", "Madrid", "Berlin", "Delhi"}
+# cities1.add("Helsinki")
+# print(cities1)
+
+# #Del function
+# del cities1
+# #print(cities1)
+
