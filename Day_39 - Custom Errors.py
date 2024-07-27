8c3b921053334c0a42769ed868f5ffd695de78c8
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-07-27 11:53:54 +0500

    Add files via upload

---
+++ Day_39 - Custom Errors.py
@@ -0,0 +1,13 @@
+# a = int(input("Enter any value between 5 and 9: "))
+
+# if (a<5 or a >9):
+#     raise ValueError ("Value should be between 5 and 9")
+
+### Quick Quiz
+
+a = input("Enter your statement here: ")
+b = ("quiz")
+if(a == b):
+    print("Your statement is correct")
+else:
+    raise ValueError("Your statement should be: quiz")
