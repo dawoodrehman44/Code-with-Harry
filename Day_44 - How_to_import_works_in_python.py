0495552a338f24c7862fe0c7a115a74014499697
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-07-31 18:03:14 +0500

    Add files via upload

---
+++ Day_44 - How_to_import_works_in_python.py
@@ -0,0 +1,20 @@
+import math
+
+# From function
+from math import sqrt, pi
+
+result=   sqrt(9) * pi
+print(result)
+
+# as function
+import pandas as pd
+#or
+import math as m
+#or
+import numpy as np
+
+# dir function
+#(dir function shows us the variables and function in the library or a bigger function)
+#print(dir(pd))
+
+from harry import welcome, harry
