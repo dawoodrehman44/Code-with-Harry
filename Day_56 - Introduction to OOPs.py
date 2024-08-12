0ef0173dd2b53fbaa2e1ed4de180825462136a00
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-08-12 21:35:39 +0500

    Add files via upload

---
+++ Day_56 - Introduction to OOPs.py
@@ -0,0 +1,17 @@
+sales1 = 6000
+profit1 = 2000
+ad1 = 1000
+
+sales2 = 6000
+profit = 2000
+ad2 = 1000
+
+sales3 = 6000
+profit = 2000
+ad3 = 1000
+
+Railwayform ---> class [blueprint]
+
+harry --> harry ki info [object]
+tom -- tom ki info [object]
+shubam --> shubam ki info [object]
