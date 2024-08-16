15c69da0b9ccfb6cc558d6ca0906e749c890a76b
Dawood Rehman <109825112+dawoodrehman44@users.noreply.github.com>
2024-08-16 12:13:24 +0500

    Add files via upload

---
+++ Day_58 - Constructors.py
@@ -0,0 +1,16 @@
+# Constructors helps in making objects
+
+class Person:
+
+    def __init__(self, name, occ):
+        print("Hey I am a person")
+        self.name = name
+        self.occ = occ
+
+    def info(self):
+        print(f"{self.name} is a {self.occ}")
+
+a = Person("Harry", "Developer")
+b = Person("Divya", "HR")
+a.info()
+b.info()
